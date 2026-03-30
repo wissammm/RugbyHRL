9ea56ff16147574c3036cbe5bcc2eb340f34bc86
Wissam Boussella <wissamboussella@gmail.com>
2026-03-30 22:41:32 +0200

    [chore] simple todo

---
+++ src/env/env.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+
+class RugbyEnv(gym.Env):
+    def __init__(self):
+        super(RugbyEnv, self).__init__()
+        
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
---
+++ src/game/engine.py
@@ -0,0 +1,12 @@
+def shortest_path():
+    pass
+
+def collision_two_circle(circle1, circle2):
+    pass
+
+def bounce():
+    pass
+
+def point_projection():
+    # TODO : projection of a point on a line (will be used to know if the there is a goal)
+    pass

49333cd66708da89478d7d8f00326ff80a388fef
wissammm <wissamboussella@gmail.com>
2026-04-07 08:54:59 +0200

    [feat] first env for curriculum [feat] basic git and project management

---
+++ src/training/behaviour/go_to_vec2.py
@@ -0,0 +1,7 @@
+import src.game.object  as object
+
+class CurriculumGoTo(): 
+    def __init__(self):
+        self.start_nb_enemy : int = 0 
+        self.start_pos : object.Vec2 = object.Vec2(0,0)
+
